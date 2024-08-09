e7af52a4f0b2a56125a23cec5f546d0d7e30e912
theomorphic <FelixMK@yandex.ru>
2024-08-09 23:04:39 +0500

    fix: comment the error

--- index.js
+++ index.js
@@ -3819,15 +3819,15 @@ bot.on("message", async(ctx)=>{
 bot.catch((err)=>{
 	const ctx = err.ctx;
 	console.error(`Error while handling update ${ctx.update.update_id}:`);
-	const e = err.error;
+	// const e = err.error;
 
-	if(e instanceof (GrammyError)){
-		console.error("Error in request:", e.description);
-	} else if( e instanceof HttpError){
-		console.error("Could not contact Telegram:", e);
-	}else{
-		console.error("Unknown error:", e);
-	}
+	// if(e instanceof (GrammyError)){
+	// 	console.error("Error in request:", e.description);
+	// } else if( e instanceof HttpError){
+	// 	console.error("Could not contact Telegram:", e);
+	// }else{
+	// 	console.error("Unknown error:", e);
+	// }
 })
 
 bot.start();
